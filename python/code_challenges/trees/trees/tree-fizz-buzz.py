b039d0f5054dea114a480a24ba91edb7c2de6a4f
saadoundhirat <saadoundhirat93@gmail.com>
2021-08-17 23:33:20 +0300

    initial submission

---
+++ python/code_challenges/trees/trees/tree-fizz-buzz.py
@@ -0,0 +1,13 @@
+###########################
+#####code challenge 18#####
+###########################
+
+# Feature Tasks
+# Write a function called fizz buzz tree
+# Arguments: k-ary tree
+# Return: new k-ary tree
+
+class K_aryTree:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
