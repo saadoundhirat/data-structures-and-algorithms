0790016a54153c8087ffff6151b445bc78366452
saadoundhirat <saadoundhirat93@gmail.com>
2021-09-05 19:40:21 +0300

    solving code challenge 31 hashmap repeated words

--- python/code_challenges/hash-tables/hash_tables/hashtable.py
+++ python/code_challenges/hash-tables/hash_tables/hashtable.py
@@ -1,7 +1,7 @@
 # implement hash tables 
 # we store data inside the hash map as Key : value pairs
 from hash_tables.linked_list import *
-
+import re
 
 class HashTable:
     def __init__(self ,size = 1024):
@@ -57,25 +57,44 @@ class HashTable:
                 del self.arr[h][idx]
     
 
+def repeated_word(str):
+    if str == "":
+        return str
+
+    strings = re.sub(r'[^\w\s]','',str).lower().split(' ')
+    print("the string is " , strings)
+
+    hash_table = HashTable()
+    for element in strings:
+        if hash_table.containsItem(element):
+            return element
+        else:
+            hash_table.addItem(element,1)
+    return 'nothing is duplicated...'
+
 
 if __name__ == "__main__":
-    table = HashTable()
-    table.addItem('march 18' , 0)
-    table.addItem('march 19' , 100)
-    table.addItem('march 20' , 200)
-    table.addItem('march 10' , 300)
-    table.addItem('march 6' , 400)
-    table.addItem('march 17' , 500)
+    # table = HashTable()
+    # table.addItem('march 18' , 0)
+    # table.addItem('march 19' , 100)
+    # table.addItem('march 20' , 200)
+    # table.addItem('march 10' , 300)
+    # table.addItem('march 6' , 400)
+    # table.addItem('march 17' , 500)
 
-    for idx in table.arr:
-        print(idx)
+    # for idx in table.arr:
+    #     print(idx)
     
-    print("======================")
-    table.addItem('march 6' , 400)    
-    print(table.containsItem('march 6'))
-    table.deleteItem('march 6')
-    table.deleteItem('march 20')
-    print(table.getItem('march 6'))
+    # print("======================")
+    # table.addItem('march 6' , 400)    
+    # print(table.containsItem('march 6'))
+    # table.deleteItem('march 6')
+    # table.deleteItem('march 20')
+    # print(table.getItem('march 6'))
 
-    for idx in table.arr:
-        print(idx)
+    # for idx in table.arr:
+    #     print(idx)
+
+    print(repeated_word("Once upon a time, there was a brave princess who..."))
+    print(repeated_word("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only..."))
+    print(repeated_word("It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn't know what I was doing in New York..."))
--- python/code_challenges/hash-tables/tests/test_hash_table.py
+++ python/code_challenges/hash-tables/tests/test_hash_table.py
@@ -1,5 +1,5 @@
 import pytest
-from hash_tables.hashtable import HashTable
+from hash_tables.hashtable import HashTable, repeated_word
 
 
 
@@ -34,3 +34,15 @@ def test_delete_item():
     table.addItem("hello" , 15)
     table.deleteItem("hello")
     assert len(table.arr[table.get_hash("hello")]) == 0
+
+
+#=========================
+# test repeated_word
+
+def test_repeated_word():
+    "test repeated_word"
+
+    assert repeated_word('') == ''
+    assert repeated_word("Once upon a time, there was a brave princess who...") == 'a'
+    assert repeated_word("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...") == 'it'
+    assert repeated_word("It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn't know what I was doing in New York...") == 'summer'
