aae4b85e44efc2cf321a5f9d322160e642508111
saadoundhirat <saadoundhirat93@gmail.com>
2021-09-21 02:08:16 +0300

    CC: Add solution for code challenge 38

--- python/code_challenges/graphs/graphs/graph.py
+++ python/code_challenges/graphs/graphs/graph.py
@@ -51,6 +51,9 @@ class Graph:
     def __init__(self):
         self._adjacency_list = {}
 
+    def print_graph(self):
+        print(self._adjacency_list)
+
     def add_node(self, value):
         """Adds value to a node and adds the node to a graph"""
         vertex_node = Vertex(value)
@@ -154,6 +157,29 @@ class Graph:
             return False,'$0'     
         return True,'$'+ str(sum)
 
+    
+    def depth_first(self, vertex=None):
+        if not vertex:
+            return[]
+        visited = set()
+        nodes = []
+
+        def rec_fun(node):
+            neighbors = self.get_neighbors(node)
+            for neighbor in neighbors:
+                node_v = neighbor.vertex
+                if node_v in visited:
+                    continue
+                else:
+                    nodes.append(node_v)
+                    visited.add(node_v)
+                    rec_fun(node_v)
+
+        nodes.append(vertex)
+        rec_fun(vertex)
+        return nodes
+
+
 
 
 if __name__ == "__main__":
@@ -172,29 +198,61 @@ if __name__ == "__main__":
     # graph.add_edge(node3, node6)
     # graph.add_edge(node4, node6)
     # print(graph.breadth_search(node1))
-    graph = Graph()
-    v1 = graph.add_node('Pandora')
-    v2 = graph.add_node('Arendelle')
-    v3 = graph.add_node('Metroville')
-    v4 = graph.add_node('Monstropolis')
-    v5 = graph.add_node('Narnia')
-    v6 = graph.add_node('Naboo')
-    graph.add_edge(v1,v2,150)
-    graph.add_edge(v1,v3,82)
-    graph.add_edge(v2,v3,99)
-    graph.add_edge(v2,v4,42)
-    graph.add_edge(v3,v4,105)
-    graph.add_edge(v3,v5,37)
-    graph.add_edge(v3,v6,26)
-    graph.add_edge(v4,v6,73)
-    graph.add_edge(v5,v6,250)
+    # graph = Graph()
+    # v1 = graph.add_node('Pandora')
+    # v2 = graph.add_node('Arendelle')
+    # v3 = graph.add_node('Metroville')
+    # v4 = graph.add_node('Monstropolis')
+    # v5 = graph.add_node('Narnia')
+    # v6 = graph.add_node('Naboo')
+    # graph.add_edge(v1,v2,150)
+    # graph.add_edge(v1,v3,82)
+    # graph.add_edge(v2,v3,99)
+    # graph.add_edge(v2,v4,42)
+    # graph.add_edge(v3,v4,105)
+    # graph.add_edge(v3,v5,37)
+    # graph.add_edge(v3,v6,26)
+    # graph.add_edge(v4,v6,73)
+    # graph.add_edge(v5,v6,250)
 
-    print(graph.print_adjacency_list())
-    cities = [v1,v2,v3]
-    print(graph.business_trip(cities))
-    cities2 = [v1,v2]
-    print(graph.business_trip(cities2))
-    cities3 = [v6,v1]
-    print(graph.business_trip(cities3))
-    cities4 = [v6]
-    print(graph.business_trip(cities4))
+    # print(graph.print_adjacency_list())
+    # cities = [v1,v2,v3]
+    # print(graph.business_trip(cities))
+    # cities2 = [v1,v2]
+    # print(graph.business_trip(cities2))
+    # cities3 = [v6,v1]
+    # print(graph.business_trip(cities3))
+    # cities4 = [v6]
+    # print(graph.business_trip(cities4))
+
+
+    graph = Graph()
+    v1 = graph.add_node('a')
+    v2 = graph.add_node('b')
+    v3 = graph.add_node('c')
+    v4 = graph.add_node('d')
+    v5 = graph.add_node('e')
+    v6 = graph.add_node('f')
+    v7 = graph.add_node('g')
+    v8 = graph.add_node('h')
+    graph.add_edge(v1, v2)
+    graph.add_edge(v1, v4)
+    graph.add_edge(v2,v1)
+    graph.add_edge(v2,v3)
+    graph.add_edge(v2,v4)
+    graph.add_edge(v3,v2)
+    graph.add_edge(v3,v7)
+    graph.add_edge(v4,v1)
+    graph.add_edge(v4,v2)
+    graph.add_edge(v4,v5)
+    graph.add_edge(v4,v8)
+    graph.add_edge(v4,v6)
+    graph.add_edge(v6,v4)
+    graph.add_edge(v6,v8)
+    graph.add_edge(v8,v6)
+    graph.add_edge(v8,v4)
+    graph.add_edge(v5,v4)
+    breadth = graph.depth_first(v1)
+    actual = [vertex.value for vertex in breadth]
+    print(actual)
+
